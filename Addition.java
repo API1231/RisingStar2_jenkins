5e9963d299cefea45e685aea1962c72e10131f95
abhay pande <abhay.pande@apiseroinc.onmicrosoft.com>
2021-06-15 15:11:45 +0530

    with scan add

--- Addition.java
+++ Addition.java
@@ -1,11 +1,14 @@
-
+import java.util.Scanner;
 
 public class Addition {
 
 	public static void main(String[] args) 
 	{	
-		int op1=50;
-		int op2=50;
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter two Numbers:");
+		int op1=sc.nextInt();
+		int op2=sc.nextInt();
 		System.out.println("Addition:"+(op1+op2));
 
 	}
