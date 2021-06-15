1c3b6679b6928824f4e24a28a14524be1d9885e2
abhay pande <abhay.pande@apiseroinc.onmicrosoft.com>
2021-06-15 14:04:46 +0530

    After addition 2

--- Addition.java
+++ Addition.java
@@ -1,15 +1,11 @@
-import java.util.Scanner;
+
 
 public class Addition {
 
 	public static void main(String[] args) 
-	{
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter first operand:");
-		int op1=sc.nextInt();
-		
-		System.out.println("Enter Second operand:");
-		int op2=sc.nextInt();
+	{	
+		int op1=50;
+		int op2=50;
 		System.out.println("Addition:"+(op1+op2));
 
 	}
